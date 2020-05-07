e3d7cdf23c2aca69d2b649bc0d314ba737316657
Apoorva Choudhary <choudhary.apoorva385@gmail.com>
2020-05-07 22:52:25 +0530

    add genre api call

--- application.py
+++ application.py
@@ -1,10 +1,19 @@
-from flask import Flask
-application = Flask(__name__)
+from app import *
+from app.models import Books, BookTags
+from app.helper import *
+from sqlalchemy.sql.expression import func
 
 
 @application.route('/')
 def index():
-    return "hello world"
+    return "<h1>WELCOME TO FLASK-AWS APP!!</h1>"
+
+
+@application.route("/<genre>")
+def genre(genre):
+    book_tag = db.session.query(BookTags).filter_by(genre=genre).subquery()
+    result = db.session.query(Books,book_tag.c.genre).join(book_tag,Books.goodreads_book_id == book_tag.c.goodreads_book_id).order_by(func.rand()).limit(10).all()
+    return books(result)
 
 if __name__ == "__main__":
      application.run(host ='0.0.0.0')
