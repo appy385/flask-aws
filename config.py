946113e9e366b46090d76ba0f6fc2261da697db1
Apoorva Choudhary <choudhary.apoorva385@gmail.com>
2020-05-07 22:56:06 +0530

    add uri

---
+++ config.py
@@ -0,0 +1,7 @@
+# Define the application directory
+import os
+BASE_DIR = os.path.abspath(os.path.dirname(__file__))
+
+# Define the database
+SQLALCHEMY_DATABASE_URI= 'mysql+pymysql://admin:admin123@flask-aws.clhw6z5lqfuy.ap-south-1.rds.amazonaws.com:3306/flaskaws'
+SQLALCHEMY_TRACK_MODIFICATIONS= False
