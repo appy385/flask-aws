096f4f2542fdd1ebd803e2280e6ba1bfd0311660
Apoorva Choudhary <choudhary.apoorva385@gmail.com>
2020-05-12 16:13:39 +0530

    mail initialize

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,9 @@
 from flask import Flask, request
 from flask_sqlalchemy import SQLAlchemy
+from flask_mail import Mail, Message
 application = Flask(__name__)
 application.config.from_object('config')
 db = SQLAlchemy(application)
+
+mail = Mail()
+mail.init_app(application)
