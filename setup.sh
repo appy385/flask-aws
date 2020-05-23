1c91e79bcfd8867ece7ce5757ac749d607a02ce2
Apoorva Choudhary <choudhary.apoorva385@gmail.com>
2020-05-23 23:29:21 +0530

    remove stop.sh

--- setup.sh
+++ setup.sh
@@ -1,14 +1,34 @@
 #!/bin/sh
 IMAGE_NAME='gunicorn_flask'
+stop(){
+  if [ $(docker ps | grep $IMAGE_NAME | wc -l) -gt 0 ]
+  then
+    docker container stop $(docker container ls -aq)
+    echo "Removing running container at once"
+  else
+    echo "No running container"
+  fi
+}
+remove(){
+  if [ $(docker images | grep $IMAGE_NAME | wc -l) -gt 0 ]
+  then
+    docker image rm $IMAGE_NAME
+  else
+    echo "no image"
+  fi
+}
+
 build() {
   docker build -t $IMAGE_NAME /home/ubuntu
 }
 run(){
    docker run --rm --detach --publish 80:80 $IMAGE_NAME
 }
-install() {
+setup() {
   echo "Installing full application at once"
+  stop
+  remove
   build
   run
 }
-install
+setup
