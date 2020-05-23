256b8c428ba572798f31095e4d62f4de4102777d
Apoorva Choudhary <choudhary.apoorva385@gmail.com>
2020-05-23 17:36:25 +0530

    add rmimage

--- stop.sh
+++ stop.sh
@@ -1,6 +1,19 @@
 #!/bin/sh
+IMAGE_NAME='gunicorn_flask'
 stop(){
   echo "Removing running container at once"
   docker container stop $(docker container ls -aq)
 }
-stop
+rmimage(){
+  echo "Removing image"
+  docker image rm $IMAGE_NAME
+
+
+}
+uninstall(){
+  echo "Removing earlier version of application"
+  stop
+  rmimage
+
+}
+uninstall
