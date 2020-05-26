fcd0b7e96692ad527c73698cdd042939f58055c4
Apoorva Choudhary <choudhary.apoorva385@gmail.com>
2020-05-26 21:17:39 +0530

    configure charset

--- config.py
+++ config.py
@@ -3,5 +3,5 @@
 # BASE_DIR = os.path.abspath(os.path.dirname(__file__))
 
 # Define the database
-SQLALCHEMY_DATABASE_URI= 'mysql+pymysql://admin:admin123@flask-aws.clhw6z5lqfuy.ap-south-1.rds.amazonaws.com:3306/flaskaws'
+SQLALCHEMY_DATABASE_URI= 'mysql+pymysql://admin:admin123@flask-aws.clhw6z5lqfuy.ap-south-1.rds.amazonaws.com:3306/flaskaws?charset=utf8mb4'
 SQLALCHEMY_TRACK_MODIFICATIONS= False
